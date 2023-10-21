b3c473c70909d9e2fc346fc8bbbf6df9e69db057
Chello20 <achilletekam8@gmail.com>
2023-10-21 17:30:48 -0400

    weather

--- Program.cs
+++ Program.cs
@@ -1,57 +1,41 @@
 using System;
-using System.Net.Http;
-using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json; // You need to install the Newtonsoft.Json NuGet package.
 
 class Program
 {
-    static async Task Main()
+    static void Main()
     {
-        string city = "New York";
-        string country = "US";
-
-        using (HttpClient client = new HttpClient())
+        string jsonFile = "weatherData.json";
+        if (File.Exists(jsonFile))
         {
-            try
-            {
-                string apiUrl = $"https://api.open-meteo.com/v1/forecast?city={city}&country={country}&daily=temperature_2m_min_max";
-                HttpResponseMessage response = await client.GetAsync(apiUrl);
+            string json = File.ReadAllText(jsonFile);
+            WeatherData data = JsonConvert.DeserializeObject<WeatherData>(json);
 
-                if (response.IsSuccessStatusCode)
-                {
-                    string json = await response.Content.ReadAsStringAsync();
-                    WeatherData data = WeatherData.FromJson(json);
-
-                    Console.WriteLine($"Weather in {city}, {country}:");
-                    Console.WriteLine($"Minimum Temperature: {data.Daily.Temperature2m.Min} °C");
-                    Console.WriteLine($"Maximum Temperature: {data.Daily.Temperature2m.Max} °C");
-                }
-                else
-                {
-                    Console.WriteLine($"Failed to retrieve weather data. Error code: {response.StatusCode}");
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"An error occurred: {ex.Message}");
-            }
+            Console.WriteLine($"Weather in {data.city}, {data.country}:");
+            Console.WriteLine($"Current Temperature: {data.temperature.current} °C");
+            Console.WriteLine($"Minimum Temperature: {data.temperature.min} °C");
+            Console.WriteLine($"Maximum Temperature: {data.temperature.max} °C");
+            Console.WriteLine($"Description: {data.description}");
+        }
+        else
+        {
+            Console.WriteLine("JSON file not found.");
         }
     }
 }
 
 public class WeatherData
 {
-    public Daily Daily { get; set; }
-
-    public static WeatherData FromJson(string json) => System.Text.Json.JsonSerializer.Deserialize<WeatherData>(json);
+    public string city { get; set; }
+    public string country { get; set; }
+    public Temperature temperature { get; set; }
+    public string description { get; set; }
 }
 
-public class Daily
+public class Temperature
 {
-    public Temperature2m Temperature2m { get; set; }
-}
-
-public class Temperature2m
-{
-    public double Min { get; set; }
-    public double Max { get; set; }
+    public double current { get; set; }
+    public double min { get; set; }
+    public double max { get; set; }
 }
